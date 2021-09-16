ed7835b17d823dec6425dd0d20faf992dba6f3c4
ZOUHEIRBN <zouheir.b97@gmail.com>
2021-09-16 16:17:37 +0100

    Fixed CORS issue

--- bindings.py
+++ bindings.py
@@ -31,14 +31,14 @@ app = Flask(__name__, template_folder='./templates', static_folder='./static')
 CORS(app)
 socket_io = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers',
-                         'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    response.headers.add('Access-Control-Allow-Credentials', 'false')
-    return response
+# @app.after_request
+# def after_request(response):
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     response.headers.add('Access-Control-Allow-Headers',
+#                          'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+#     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+#     response.headers.add('Access-Control-Allow-Credentials', 'false')
+#     return response
 
 @app.route('/', methods=["GET"])
 def default_text():
